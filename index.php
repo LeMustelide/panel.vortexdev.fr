d67c92c3a670329052566a0904a204e10724222f
FireFull45 <firefull2945@gmail.com>
2022-01-25 14:39:47 +0100

    Modification de l'index (test)

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
-header("refresh: 5; https://panel.vortexdev.fr/public/index.php");
+require 'vendor/autoload.php';
+// index.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-	echo '<title>Symfony Installed</title><div style="background: #e9ffed; border: 1px solid #b0dab7; padding: 15px;" align="center" >
-	<font size="5" color="#182e7a">Symfony is installed successfully.</font><br /><br />
-	<font size="4">Symfony is a Framework and doesn\'t have an index page.<br /><br />
-	You will be redirected to its "public" folder in 5 seconds...<br /><br />
-	Symfony aims to speed up the creation and maintenance of web applications, and to replace the repetitive coding tasks by power, control and pleasure.
+$request = Request::createFromGlobals();
+$url = $request->getPathInfo();
+$response = new Response();
 
-Symfony is aimed at building robust applications in an enterprise context. This means that you have full control over the configuration: from the directory structure to the foreign libraries, almost everything can be customized. </font></div>';
-?>
+switch($url) {
+    case '/':
+        $response->setContent('Accueils');
+        break;
+    case '/admin':
+        $response->setContent('Accès Espace Admin');
+        break;
+    default:
+        $response->setStatusCode(Response::HTTP_NOT_FOUND);
+}
+
+$response->send();
