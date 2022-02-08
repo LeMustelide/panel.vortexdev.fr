39027b4b498034b7cab641d23a63cb415a393319
Tom <venom4572@gmail.com>
2022-02-08 20:02:03 +0100

    pop supression clées

--- public/Js/keys.js
+++ public/Js/keys.js
@@ -1,9 +1,9 @@
 
-
 function supprimerKey(id){
+    console.log(id);
     nb = id.slice(3);
-    keys =document.getElementsByTagName('table')[0].getElementsByTagName('tr')[nb].cells[1].innerHTML;
-    console.log(keys);
+    keys = document.getElementsByTagName('table')[0].getElementsByTagName('tr')[nb].cells[1].innerHTML;
+    // console.log(keys);
     $.ajax({
         url: "/keys/supKeys",
         type : "POST",
@@ -15,12 +15,19 @@ function supprimerKey(id){
             console.log(keys);
         },
     });
+    elem =document.getElementById("key"+nb);
+    elem.remove();
 }
 
-function supprimerKey2(id)
+
+function openForm(id)
 {
-    nb = id.slice(3);
-    elem =document.getElementById("key"+nb);
-   
-    elem.remove();
+    console.log('supprimerKey('+id+')');
+    document.getElementById("action").setAttribute('onclick','supprimerKey(\''+id+'\'),closeForm()');
+    document.getElementById("deleteForm").style.display = "block";
+}
+
+function closeForm() 
+{
+    document.getElementById("deleteForm").style.display = "none";
 }
