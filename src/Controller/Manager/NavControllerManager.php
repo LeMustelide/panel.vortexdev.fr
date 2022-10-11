3c8960e7c1c9771d50969cc9216e9f62d0342914
FireFull45 <firefull2945@gmail.com>
2022-10-11 22:06:30 +0200

    patch

--- src/Controller/Manager/NavControllerManager.php
+++ src/Controller/Manager/NavControllerManager.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller\Manager;
 
+use App\Repository\panel\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class NavControllerManager extends AbstractController
 {
-
     #[Route('/logout0', name: 'logout0')]
-    public function logout()
+    public function logout(int $size, int $page, UserRepository $Users, PaginatorInterface $paginator, Request $request)
     {
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+
+        $usersList = $Users->findAll();
+
+        $pagination = $paginator->paginate(
+            $usersList, /* query NOT result */
+            $request->query->getInt('page', $page), /*page number*/
+            $size /*limit per page*/
+        );
+        return $this->render('manager.html.twig', []);
     }
 }
